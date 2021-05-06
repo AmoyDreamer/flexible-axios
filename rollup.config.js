408c74b9ff953bbae487afa3a2f8298b5a5ad7ad
Allen <378611380@qq.com>
2021-05-07 01:34:54 +0800

    update rollup.config.js file

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,30 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
 import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { babel } from '@rollup/plugin-babel';
 import { uglify } from 'rollup-plugin-uglify';
 
-export default {
+let config = {
     input: 'src/index.js',
     output: [{
         file: './dist/index.min.js',
-        format: 'umd',
+        format: 'iife',
         name: 'flexibleAxios'
     }],
     plugins: [
         commonjs(),
         nodePolyfills(),
-        nodeResolve(),
+        nodeResolve({ mainFields: ["jsnext", "preferBuiltins", "browser"] }),
         json(),
-        uglify()
+        babel({
+            "exclude": 'node_modules/**',
+            "babelHelpers": 'bundled',
+            "presets": ['@babel/preset-env'],
+        })
     ]
-};
+}
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(uglify())
+}
+export default config;
